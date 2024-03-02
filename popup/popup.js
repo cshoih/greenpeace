941fef806414526ec8d11dc9e6094ff5b67db2b1
cshoih <cinghoih.04@gmail.com>
2024-03-02 11:20:39 +1100

    stuff

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var captureBtn = document.getElementById('captureBtn');
-  
-    captureBtn.addEventListener('click', function() {
-      chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(dataUrl) {
-        var img = document.createElement('img');
-        img.src = dataUrl;
-        document.body.appendChild(img);
-      });
+  var captureBtn = document.getElementById('captureBtn');
+
+  captureBtn.addEventListener('click', function() {
+    chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(dataUrl) {
+      var img = document.createElement('img');
+      img.src = dataUrl;
+      document.body.appendChild(img);
     });
   });
+});
+
