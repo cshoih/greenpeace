04b2025b48d843e5fab4d00c597ef0765c437c72
cshoih <cinghoih.04@gmail.com>
2024-03-02 11:53:39 +1100

    ...idk

--- popup/background.js
+++ popup/background.js
@@ -1,1 +1,8 @@
-// Background script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'takeScreenshot') {
+      chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(dataUrl) {
+        chrome.runtime.sendMessage({action: 'displayScreenshot', screenshotDataUrl: dataUrl});
+      });
+    }
+  });
+  
--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   var captureBtn = document.getElementById('captureBtn');
-
   captureBtn.addEventListener('click', function() {
-    chrome.tabs.captureVisibleTab(null, {format: 'png'}, function(dataUrl) {
-      var img = document.createElement('img');
-      img.src = dataUrl;
-      document.body.appendChild(img);
-    });
+    chrome.runtime.sendMessage({action: 'takeScreenshot'});
   });
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === 'displayScreenshot') {
+    var screenshotContainer = document.getElementById('screenshotContainer');
+    screenshotContainer.innerHTML = '<img src="' + message.screenshotDataUrl + '" alt="Screenshot">';
+  }
 });
 
